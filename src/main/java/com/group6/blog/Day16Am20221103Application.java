07b4257e755ead60f60de0f3534730c6c4a517cc
RigHakiko <add-122@hotmail.com>
2022-11-03 23:17:07 +0900

    生成了代码

--- src/main/java/com/group6/blog/Day16Am20221103Application.java
+++ src/main/java/com/group6/blog/Day16Am20221103Application.java
@@ -1,9 +1,11 @@
 package com.group6.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.group6.blog.mapper")
 public class Day16Am20221103Application {
 
     public static void main(String[] args) {
--- src/test/java/com/group6/blog/Day16Am20221103ApplicationTests.java
+++ src/test/java/com/group6/blog/Day16Am20221103ApplicationTests.java
@@ -1,5 +1,11 @@
 package com.group6.blog;
 
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +16,50 @@ class Day16Am20221103ApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void generate(){
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("朱虹运20221103");
+        gc.setOpen(false);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/20221102_day15_pm_project");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.group6.blog");
+        mpg.setPackageInfo(pc);
+
+
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+
+        // 写于父类中的公共字段
+
+
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+
+        mpg.execute();
+    }
 }
